b8089eb72ed1f4c81054c4a47abc0861c108667b
Tuvshuu1211 <tuvshuu1211@gmail.com>
2020-06-16 15:28:12 +0800

    updates

--- js/script.js
+++ js/script.js
@@ -39,24 +39,67 @@
     }
 })
 
-  $(document).ready(function(){
-    $('.accordion').each(function(){
-      if($(this).is('.is-active')){
-        $(this).find('.accordion-body').slideDown();
-      }
-    })
-  })
-  $(".accordion-header").click(function(){
-    $('.accordion').each(function(){
-      $(this).removeClass('is-active')
-      if($(this).not('.is-active')){
-        $(this).find('.accordion-body').slideUp();
-      }
-    })
-      $(this).next().slideDown();
+
+function accHandler(){
+  $('.accordion').each(function(){
+  if($(this).is('.is-active')){
+    $(this).find('.accordion-body').slideDown();
+  }else{
+    $(this).find('.accordion-body').slideUp();
+  }
+})
+}
+
+ $(document).ready(function(){
+  accHandler();
+  $('.accordion').each(function(){
+    
+    $(this).find('.accordion-header').on('click', function(){
       $(this).parent().toggleClass('is-active');
+      accHandler()
+    })
+    
+  })
+ })
 
-  });
+ $('.book-slider').owlCarousel({
+  center: true,
+  loop: true,
+  nav: true,
+  margin: 0,
+  items: 5,
+  navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+  responsive:{
+    0:{
+      items: 2,
+    },
+    768:{
+      items: 3,
+    },
+    990:{
+      items: 5,
+    }
+  },
+  onInitialized: coverFlowEfx,
+  onTranslate: coverFlowEfx,
+});
 
+function coverFlowEfx(e){
+  if ($('.owl-dots')) {
+    $('.owl-dots').remove();
+  }
+  var idx = e.item.index;
+  $('.owl-item.big').removeClass('big');
+  $('.owl-item.medium').removeClass('medium');
+  $('.owl-item.mdright').removeClass('mdright');
+  $('.owl-item.mdleft').removeClass('mdleft');
+  $('.owl-item.smallRight').removeClass('smallRight');
+  $('.owl-item.smallLeft').removeClass('smallLeft');
+  $('.owl-item').eq(idx -1).addClass('medium mdleft');
+  $('.owl-item').eq(idx - 2).addClass('smallLeft');
+  $('.owl-item').eq(idx).addClass('big');
+  $('.owl-item').eq(idx + 1).addClass('medium mdright');
+  $('.owl-item').eq(idx + 2).addClass('smallRight');
+}
 
 })(jQuery); // End of use strict
